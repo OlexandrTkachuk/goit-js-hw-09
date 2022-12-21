ed2791c7bd90416e9415f8086bb1ba416a7776d9
Olexandr Tkachuk <temishena@gmail.com>
2022-12-21 14:42:30 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+import '../css/common.css';
 
+const refs = {
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.startButton.addEventListener('click', onStartButtonClick);
+refs.stopButton.addEventListener('click', onStopButtonClick);
+
+const DELAY = 1000;
+let timerId = null;
+
+function onStartButtonClick() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+  console.log('start');
+}
+
+function onStopButtonClick() {
+  clearInterval(timerId);
+  console.log('stop');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
