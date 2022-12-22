31234b905cfdd49c4300cb2fb1c72ed74f45f0e6
Olexandr Tkachuk <temishena@gmail.com>
2022-12-22 19:47:23 +0200

    second task

---
+++ src/js/02-timer.js
@@ -0,0 +1,77 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.startBtn.disabled = true;
+
+let ms = 0;
+let selectedTime = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() < Date.now()) {
+      Notify.failure('😪 ... Please choose a date in the future');
+      return;
+    }
+
+    refs.startBtn.disabled = false;
+    selectedTime = selectedDates[0].getTime();
+  },
+};
+
+flatpickr(refs.input, options);
+
+function onStartBtnClick() {
+  const intervalId = setInterval(() => {
+    if (selectedTime < Date.now()) {
+      clearInterval(intervalId);
+
+      return;
+    }
+    ms = selectedTime - Date.now();
+    refs.startBtn.disabled = true;
+
+    console.log(convertMs(ms));
+    render(convertMs(ms));
+  }, 1000);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = pad(Math.floor(ms / day));
+  const hours = pad(Math.floor((ms % day) / hour));
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function render({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
